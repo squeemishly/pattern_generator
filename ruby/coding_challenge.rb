7f8f651f563fca3bfa575ffe2176d9d20136bd10
squeemishly <squeemishly@gmail.com>
2017-09-29 12:11:00 -0600

    refactors solution for SRP methods

--- ruby/coding_challenge.rb
+++ ruby/coding_challenge.rb
@@ -3,14 +3,22 @@ require 'pry'
 class PatternGenerator
   def verify(seq, code)
     aggregated = seq.chars.zip(code.chars)
+    return false if !determine_mismatch(aggregated)
+    true
+  end
+
+  def determine_mismatch(aggregated)
     aggregated.each do |set|
       return false if alpha_mismatch(set)
-      return false if set[0] =~ /[[:digit:]]/ && set[1] == "."
+      return false if digit_mismatch(set)
     end
-    true
   end
 
   def alpha_mismatch(set)
     set[0] =~ /[[:alpha:]]/ && set[1] == "#"
+  end
+
+  def digit_mismatch(set)
+    set[0] =~ /[[:digit:]]/ && set[1] == "."
   end
 end
