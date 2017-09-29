d13831274c3396ba88b83e8637fb4a440beb5936
squeemishly <squeemishly@gmail.com>
2017-09-29 12:03:18 -0600

    solves basic verification of letters and numbers

--- ruby/coding_challenge.rb
+++ ruby/coding_challenge.rb
@@ -1,4 +1,16 @@
 require 'pry'
 
-class Object
+class PatternGenerator
+  def verify(seq, code)
+    aggregated = seq.chars.zip(code.chars)
+    aggregated.each do |set|
+      return false if alpha_mismatch(set)
+      return false if set[0] =~ /[[:digit:]]/ && set[1] == "."
+    end
+    true
+  end
+
+  def alpha_mismatch(set)
+    set[0] =~ /[[:alpha:]]/ && set[1] == "#"
+  end
 end
--- ruby/coding_challenge_test.rb
+++ ruby/coding_challenge_test.rb
@@ -4,10 +4,29 @@ require 'minitest/autorun'
 require_relative './coding_challenge'
 
 class ObjectTest < Minitest::Test
-  def test_it_exists
-    
-    template = TemplateClass.new
-    assert_instance_of TemplateClass, template
+  attr_reader :pg
 
+  def setup
+    @pg = PatternGenerator.new
+  end
+
+  def test_it_exists
+    assert_instance_of PatternGenerator, pg
+  end
+
+  def test_it_verifies_a_single_letter
+    assert_equal true, pg.verify("A", ".")
+  end
+
+  def test_it_returns_false_for_a_bad_pattern
+    assert_equal false, pg.verify("A", "#")
+  end
+
+  def test_it_confirms_a_more_complicated_pattern
+    assert_equal true, pg.verify("A3B", ".#.")
+  end
+
+  def test_it_refutes_a_more_complicated_pattern
+    assert_equal false, pg.verify("ABB", ".#.")
   end
 end
